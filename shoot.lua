791be90105d320474b369d7b4f8378432407db8e
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2016-12-30 22:32:37 +0100

    initial commit

---
+++ shoot.lua
@@ -0,0 +1,26 @@
+local shooting
+
+local f = CreateFrame'Frame'
+f:RegisterEvent'START_AUTOREPEAT_SPELL'
+f:RegisterEvent'STOP_AUTOREPEAT_SPELL'
+f:SetScript('OnEvent', function()
+	shooting = event == 'START_AUTOREPEAT_SPELL'
+end)
+
+SLASH_SHOOT1 = '/shoot'
+function SlashCmdList.SHOOT(command)
+	if not shooting then
+		CastSpellByName'Auto Shot'
+	end
+end
+
+f:SetScript('OnUpdate', function()
+	f:SetScript('OnUpdate', nil)
+	for i = 1, GetNumMacros() do
+		if GetMacroInfo(i) == 'shoot' then
+			EditMacro(i, 'shoot', 55, '/shoot', 1)
+			return
+		end
+	end
+	CreateMacro('shoot', 55, '/shoot', 1)
+end)
